076cb01f8e240fab91cd14f9ba086bcfa7c2e4e4
Luca Maria Incarnato <lucaincarnato.03@gmail.com>
2025-10-08 12:21:36 +0200

    Initial Commit

---
+++ ToiletApp/ToiletAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToiletAppApp.swift
+//  ToiletApp
+//
+//  Created by Luca Maria Incarnato on 08/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ToiletAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
